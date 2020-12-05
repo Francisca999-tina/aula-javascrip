583da2bdada3a808092f091019186eca7cd0ab66
Francisca999-tina <68696276+Francisca999-tina@users.noreply.github.com>
2020-12-05 10:54:55 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,18 @@
 
 
-var nome = "Fran Costa";
-var idade = 35;
-var idade2 = 1
-//alert (nome + "tem" + idade + "anos");
-alert(idade + idade2);
+
+var lista = ["maça", "pêra", "laranja"];
+
+console.log(lista[1]);
+
+
+//var nome = "Fran Costa";
+var n1 = 5;
+var n2 = 3;
+var frase = "Barcelona é o melhor time do mundo";
+//alert(nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.replace("Barcelona", "Brasil "));
+//alert (frase.replace ("Barcelona", "Brasil"));
