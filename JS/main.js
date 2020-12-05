0550c87872c2017dee9129cee738187335c2e767
Francisca999-tina <68696276+Francisca999-tina@users.noreply.github.com>
2020-12-05 12:39:07 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,28 @@
 
 
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"} ]
+console.log(frutas);
+alert(frutas[1].nome);
 
-var lista = ["maça", "pêra", "laranja"];
 
-console.log(lista[1]);
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop()
+
+//console.log(lista[1]);
 
 
 //var nome = "Fran Costa";
-var n1 = 5;
-var n2 = 3;
-var frase = "Barcelona é o melhor time do mundo";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Barcelona é o melhor time do mundo";
 //alert(nome + "tem" + idade + "anos");
 //alert(idade + idade2);
 //console.log(nome);
